917779fdc3362e73c1aa5208886d54875883d988
AndreDwiF <77494322+AndreDwiF@users.noreply.github.com>
2022-01-13 23:28:38 +0700

    Add files via upload
    "Progress Login"

---
+++ view_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+</head>
+<body>
+   <div class="container">
+          <h1>Login</h1>
+          <form action="<?php echo base_url('Login/login'); ?>"method="post">
+                <label>Username</label><br>
+                <input type="text" name="username"><br>
+                 <label>Password</label><br>
+                <input type="password" name="password"><br>
+          
+                <button type=submit >Log in</button>
+            </form>
+        </div>       
+
+</body>
+</html>
+  
+     
+
+     
